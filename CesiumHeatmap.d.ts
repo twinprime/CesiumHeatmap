71b4bf583bdade2700c3ebb8ac4ef3c096645acb
choya <leeys.1@gmail.com>
2018-09-30 19:47:36 +0800

    Add type definitions Support entities only

---
+++ CesiumHeatmap.d.ts
@@ -0,0 +1,23 @@
+export namespace CesiumHeatmap {
+  interface Bounds {
+    west: number, east: number, south: number, north: number
+  }
+  interface Options {
+		minCanvasSize?: number,          // minimum size (in pixels) for the heatmap canvas
+		maxCanvasSize?: number,          // maximum size (in pixels) for the heatmap canvas
+		radius?: number,
+		radiusFactor?: number,           // data point size factor used if no radius is given (the greater of height and width divided by this number yields the used radius)
+		spacingFactor?: number,          // extra space around the borders (point radius multiplied by this number yields the spacing)
+		maxOpacity?: number,             // the maximum opacity used if not given in the heatmap options object
+		minOpacity?: number,             // the minimum opacity used if not given in the heatmap options object
+		blur?: number,                   // the blur used if not given in the heatmap options object
+		gradient?: object                // the gradient used if not given in the heatmap options object
+	}
+	interface DataPoint {
+		x: number, y: number, value: number
+	}
+	interface Instance {
+		setWGS84Data<T>(min: number, max: number, data: DataPoint[]): T?
+	}
+  function create(bb: Bounds, options: Options): Instance
+}
